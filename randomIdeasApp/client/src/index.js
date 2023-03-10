545487131267ceef37f525d853408ea15e0b0bc8
Michael Brockman <mbrockmandev@gmail.com>
2023-03-10 08:44:36 -0500

    prep for deploy

--- randomIdeasApp/client/src/index.js
+++ randomIdeasApp/client/src/index.js
@@ -1,4 +1,4 @@
-import '@fortawesome/fontawesome-free/css/all.css';
+// import '@fortawesome/fontawesome-free/css/all.css';
 import IdeaForm from './components/IdeaForm';
 import './css/style.css';
 import Modal from './components/Modal';
